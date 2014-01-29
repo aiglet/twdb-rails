268b6598708959c394fbec9283f0b878c06e6008
Kate Secor <aiglet@gmail.com>
2014-01-29 12:53:47 -0800

    Auth work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140129012735) do
+ActiveRecord::Schema.define(:version => 20140129203951) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(:version => 20140129012735) do
     t.datetime "updated_at",            :null => false
     t.string   "extension"
     t.boolean  "no_ISBN"
+  end
+
+  create_table "identities", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "people", :force => true do |t|
